aa93630d61b6294693154af5fb806ae9cf884893
Bahubalendra <bahubalendra.barik@gmail.com>
2021-04-01 11:42:33 +0530

    first commit

---
+++ Password_generated.py
@@ -0,0 +1,14 @@
+#it's work
+import random
+import string
+
+print('Hello, welcome to password generator....')
+length = int(input('\nEnter the length of password: '))
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
+num = string.digits
+symbols = string.punctuation
+all = lower + upper + num + symbols
+temp = random.sample(all, length)
+password = "".join(temp)
+print(password)
